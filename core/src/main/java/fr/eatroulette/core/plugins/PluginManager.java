ffc37241d94f5f8e20f3075a0bd118a120bde259
Gwen <gwendou95@hotmail.fr>
2020-05-23 13:41:33 +0200

    :sparkles: Add loadPluginFunction

--- core/src/main/java/fr/eatroulette/core/plugins/PluginManager.java
+++ core/src/main/java/fr/eatroulette/core/plugins/PluginManager.java
@@ -1,13 +1,40 @@
 package fr.eatroulette.core.plugins;
 
+import java.io.File;
 import java.util.Enumeration;
 import java.util.jar.JarFile;
 
 public class PluginManager {
+    private File pluginFolder;
+    private File[] listOfjars;
 
 
+    public PluginManager(){
+        this.pluginFolder = new File("plugin/");
+        loadAllJar();
+    }
 
-    public static void runPlugin(String pathPlugin) throws Exception {
+    public void loadAllJar(){
+        this.listOfjars = this.pluginFolder.listFiles((dir, name) -> name.endsWith(".jar"));
+
+        for (File f: this.listOfjars){
+            System.out.println(f.getPath());
+//            if (f.isFile()){
+//                String fullyQualifiedName = f.getPath()
+//                        .replace("lib\\", "")
+//                        .replace("\\", ".")
+//                        .replace(".class", "");
+//
+//                listClasses.add(fullyQualifiedName);
+//            } else if(f.isDirectory()){
+////                listClasses = findClasses(f);
+//            }
+        }
+
+    }
+
+
+    public void runPlugin(String pathPlugin) throws Exception {
 //        if(pathPlugin.isEmpty() || pathPlugin.isBlank()){
 //            throw new Exception("File path does not exist");
 //        }
